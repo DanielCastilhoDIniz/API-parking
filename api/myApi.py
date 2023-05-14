cba1e92f60f3eaaaf21809f7c05a6e9bbfde03c3
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-05-14 02:09:01 -0300

    inicio da api

--- api/myApi.py
+++ api/myApi.py
@@ -12,30 +12,22 @@ api_key = load_key['GOOGLE_MAPS_API_KEY']
 
 @app.route('/api/places', methods=['GET'])
 def get_nearbay_places():
-    lat = request.args.get('lat')
-    lng = request.args.get('lng')
-
-    # Verifica se as coordenadas foram fornecidas corretamente
-    if not lat or not lng:
-        return jsonify({'error': 'As coordenadas devem ser fornecidas.'}), 400
 
     # Faz a consulta do CEP para obter o endereço completo
-    cep = request.args.get('cep')
+    # cep = request.args.get('cep')
+    cep = 58051200
     if cep:
         cep_url = f"https://cep.awesomeapi.com.br/json/{cep}"
         cep_response = requests.get(cep_url)
         endereco_dic = cep_response.json()
+        lat = endereco_dic['lat']
+        lng = endereco_dic['lng'] 
 
         if endereco_dic.get('error'):
             return jsonify({'error': endereco_dic['error']}), 400
-
-        # Usa a cidade e o estado para refinar a busca do Google Places
-        cidade = endereco_dic['city']
-        estado = endereco_dic['state']
-        location = f"{cidade}, {estado}"
-    else:
-        # Se o CEP não foi fornecido, usa a latitude e a longitude diretamente
-        location = f"{lat},{lng}"
+        else:
+            # Se o CEP não foi fornecido, usa a latitude e a longitude diretamente
+            location = f"{lat},{lng}"
 # Faz a solicitação à API do Google Places para obter os locais próximos
     url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={location}&radius=1000&type=bar&language=pt-BR&key={api_key}"
     response = requests.get(url)
